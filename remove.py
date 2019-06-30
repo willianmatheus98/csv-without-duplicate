c1848f17c5da0df3a3379a5c049db23192c230d2
wilaman21 <williantecnico1998@gmail.com>
2019-06-29 22:54:10 -0300

    Create remove.py

---
+++ remove.py
@@ -0,0 +1,5 @@
+import pandas as pd
+file_name = "C:\\Users\\Nome do Usuário\\Desktop\\file_with_dupes.csv"
+file_name_output = "C:\\Users\\Nome do Usuário\\Desktop\\my_file_without_dupes.csv"
+file_name_output2 = "C:\\Users\\Nome do Usuário\\Desktop\\my_file_without_dupes2.csv"
+
